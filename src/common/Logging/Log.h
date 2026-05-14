10eac5595374630d8c70871ce7b8c6935bfe6f13
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-05-14 18:47:01 -0400

    Comment out unused _strand member in Log.h
    Comment out the unused _strand member variable in Log class.

--- src/common/Logging/Log.h
+++ src/common/Logging/Log.h
@@ -125,7 +125,7 @@ private:
     uint32 realm{};
     boost::asio::io_service* _ioService;
     boost::asio::strand<boost::asio::io_context::executor_type>* _strand;
-#    boost::asio::strand* _strand;
+//    boost::asio::strand* _strand;
 };
 
 #define sLog Log::instance()
